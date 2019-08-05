8e64af379aa415e96644a82b84a534be1c644625
Syd Bailey <sydabailey@gmail.com>
2019-08-05 03:19:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  dwarf_names.each_with_index do |name, i|
+   puts "#{i+1} #{name}"
+ end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  new_call = []
+planeteer_calls.collect do |call|
+new_call << call.capitalize
+end
+new_call.collect do |x|
+x + "!"
+end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any? do |word|
+word.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
